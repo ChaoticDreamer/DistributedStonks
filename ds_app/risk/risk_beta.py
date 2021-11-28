82138be9e662f3519230044ad6a531146ba11392
jeipollack <jpollacke@gmail.com>
2021-11-28 17:30:33 +0100

    Created a folder ds_app contained modules: ui, stocks, and risk

---
+++ ds_app/risk/risk_beta.py
@@ -0,0 +1,11 @@
+# This file is part of meas.
+#
+# Developed for the Code With Me Distributed Stonks Project.
+#
+import numpy as np
+
+def est_risk():
+	print("Risk is high.")
+
+if __name__ == "__main__":
+	est_risk()
---
+++ ds_app/ui/user_interface.py
@@ -0,0 +1,9 @@
+# This file is part of ui.
+#
+# Developed for the Code With Me Distributed Stonks Project.
+#
+from ..risk.risk_beta import est_risk
+
+
+if __name__ == "__main__":
+	est_risk()
