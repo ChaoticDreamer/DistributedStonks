9f08ff85a94ce1c9e20ea4a54c580d7bd6c0baea
jeipollack <jpollacke@gmail.com>
2021-11-28 23:28:31 +0100

    Testing relative imports with streammlit

--- ds_app/ui/user_interface.py
+++ ds_app/ui/user_interface.py
@@ -2,8 +2,16 @@
 #
 # Developed for the Code With Me Distributed Stonks Project.
 #
-from ..risk.risk_beta import est_risk
+from risk.risk_beta import est_risk
+import streamlit as st
 
+def get_risk_for_stock():
+	if est_risk() == "High":
+	   st.title("Stock Overvalued")
+
+def set_title():
+	st.title("Distributed Stoncks Risk Evaluator")
+	st.text_input("Enter the stock name for risk assessment")
 
 if __name__ == "__main__":
 	est_risk()
