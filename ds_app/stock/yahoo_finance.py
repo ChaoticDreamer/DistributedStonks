ab2142d09491ce7277a10cff030e3d225c6815f2
jeipollack <jpollacke@gmail.com>
2021-12-03 22:56:26 +0100

    Added CompanyInfo Class

--- ds_app/stock/yahoo_finance.py
+++ ds_app/stock/yahoo_finance.py
@@ -2,17 +2,33 @@
 #
 # Developed for the Code With Me Distributed Stonks Project.
 #
-
+import sys
+import re
 import yahoo_fin.stock_info as si
 
-
-def get_info(stock_ticker):
-   stock = si.get_data(stock_ticker)
-   print(stock)
-
-
-
-
-if __name__ == "__main__":
-	get_info("MSFT")
+class CompanyInfo:
 	
+   def __init__(self,ticker):
+      try:
+         self.ticker=ticker
+         self.price_df = si.get_data(self.ticker) 
+         self.overview_df = si.get_stats(self.ticker) 
+         self.get_earnings = si.get_earnings(self.ticker)
+         self.get_financials = si.get_financials(self.ticker)
+         self.get_quote_data = si.get_quote_data(self.ticker)
+         self.get_stats_df = si.get_stats(self.ticker)
+         self.get_stats_valuation = si.get_stats_valuation(self.ticker)
+         self.overviewQuote_dict = si.get_quote_table(self.ticker)        
+         self.income_statement_df = si.get_income_statement(self.ticker)
+         self.balance_sheet_df = si.get_balance_sheet(self.ticker)
+         self.cash_flows_df = si.get_cash_flow(self.ticker)  
+      except:
+         e=sys.exc_info()[0]
+         # Wait for 5 seconds, then try call again in case of Network hicup
+         #time.sleep(5)
+         raise e
+
+       
+if __name__ == "__main__":
+   msft_stock = CompanyInfo('msft')
+   print(msft_stock.overview_df)	
