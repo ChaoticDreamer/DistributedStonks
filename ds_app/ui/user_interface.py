0e37a026d8f48a84f038c9b183e951f8d9283df6
jeipollack <jpollacke@gmail.com>
2021-12-03 22:57:43 +0100

    Removed set_title() and added format_text_color()

--- ds_app/ui/user_interface.py
+++ ds_app/ui/user_interface.py
@@ -2,16 +2,43 @@
 #
 # Developed for the Code With Me Distributed Stonks Project.
 #
-from risk.risk_beta import est_risk
+import re
+from risk.risk_fairvalue import evaluate_ticker
 import streamlit as st
+import pandas as pd
 
-def get_risk_for_stock():
-	if est_risk() == "High":
-	   st.title("Stock Overvalued")
+def get_ticker_input():
+   ticker_input = st.sidebar.text_input(label="Please Enter Company's Ticker:",value="")
+   if(st.sidebar.button("Submit") and ticker_input!=""):
+      check_and_evaluate_ticker_input(ticker_input)
+      
+def check_and_evaluate_ticker_input(ticker_input):	
+   ticker=ticker_input.title().lower()
+   if re.search(r"\d",ticker) or re.search(r"\W",ticker):
+      st.error("Ticker entered is invalid. Please Enter Correctly the Company's Ticker")   
+   else:
+      currentValue, fairValue, stockAssessment = evaluate_ticker(ticker)
+      format_stockAssessment = format_text_color(stockAssessment)
 
-def set_title():
-	st.title("Distributed Stoncks Risk Evaluator")
-	st.text_input("Enter the stock name for risk assessment")
+      stock_pdf = pd.DataFrame([(ticker,currentValue,fairValue,stockAssessment)],
+      	columns=('Ticker','Current Value', 'Fair Value', 'Stock Valuation'))
+
+      styler = stock_pdf.style.hide_index().applymap(lambda x:format_stockAssessment, subset=['Stock Valuation'])
+      st.markdown("""### **Company's Stock Evaluation using Fair Value Estimator**""")
+     
+      st.write(styler.to_html(), unsafe_allow_html=True)
+
+def format_text_color(stockAssessment):
+
+   if stockAssessment=='Overvalued':
+      color = 'font-family: "Times New Roman", Times, serif; color: #e83e8c; font-size:1.3em;'
+   elif stockAssessment=='Undervalued':
+      color = 'font-family: "Times New Roman", Times, serif; color: #3ee845; font-size:1.3em;'           
+   else: 
+      color = 'font-family: "Times New Roman", Times, serif; color: #7a7a7a; font-size:1.3em;'      
+   
+   return color
+
 
 if __name__ == "__main__":
-	est_risk()
+   get_ticker_input()
