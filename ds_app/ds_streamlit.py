35aa48fc3e647f043fcabd6b2e4ca1b56ef1ed6f
jeipollack <jpollacke@gmail.com>
2021-12-03 22:55:37 +0100

    Changed title name

--- ds_app/ds_streamlit.py
+++ ds_app/ds_streamlit.py
@@ -10,10 +10,32 @@ import sys
 sys.path.append(str(pathlib.Path().absolute()).split("/ds_app")[0] + "/ds_app")
 
 import streamlit as st
-from ui.user_interface import set_title, get_risk_for_stock
+import ui.user_interface as ui
 
+HTML = """<!DOCTYPE html>
+<html>
+<head>
+<title>Code_With_Me Using Python</title>
+<style>
+    h1{
+        text-align: center;
+    }
+    img {
+        width:100%;
+        height:100%;
+    } 
+</style
+</head>
+<h1>
+    <i> 
+        <font size="9" face ="verdana" color ="lightblue" margin:5px>Distributed Stoncks </br> Risk Evaluator</font>
+    </i>  
+</h1> 
+<img src="https://res.cloudinary.com/emerline/image/upload/v1605796379/zj4tre4h0yf3eefwg8u7.jpg" alt="res.cloudinary.com">     
+</body>
+</html>"""
 
+st.write(HTML,unsafe_allow_html=True)
 
 if __name__ == "__main__":
-	set_title()
-	get_risk_for_stock()
+   ui.get_ticker_input()
